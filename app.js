0118ce39c8661fe812a2dcbe5d474074920e146d
Derek <derekdanielson84@gmail.com>
2023-07-13 09:32:33 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+// Create a promise
 const promise = new Promise((resolve, reject) => {
-    setTimeout();
+    // Do some async task
+    setTimeout(() => {
+        console.log('Async task complete');
+        resolve();
+    }, 1000);
 });
+
+promise.then(() => {
+    console.log('Promise consumed');
+});
+
+new Promise((resolve, reject) => {
+    // Do some async task
+    setTimeout(() => {
+        
+        resolve({ name: 'John', age: 30});
+    }, 1000);
+}).then((user) => console.log(user));
+
+
+console.log('Global scope');
